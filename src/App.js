8e1ddb254c39802b74538d9b8f311c28178c979c
laptop <juha.jylhakoski@aalto.fi>
2021-07-11 14:42:31 +0800

    5.1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,105 @@
 // $ npm install
 
 import React, { useState, useEffect } from 'react'
+
 import Blog from './components/Blog'
+
 import blogService from './services/blogs'
+
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newBlog, setNewBlog] = useState('')
+  const [showAll, setShowAll] = useState(false)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
+
+  const handleBlogChange = (event) => {
+
+    console.log(event.target.value)
+
+    setNewBlog(event.target.value)
+  }
+
+  const blogsToShow = showAll
+  ? blogs
+  : blogs.filter(blog => blog.important)
+
+  const handleLogin = async (event) => {
+
+    event.preventDefault()
+
+    try {
+
+      const user = await loginService.login({username, password,})
+
+      console.log('Login: ', username, password)
+
+      blogService.setToken(user.token)
+      
+      setUser(user)
+
+      setUsername('')
+
+      setPassword('')
+
+    } catch (exception) {
+
+      setErrorMessage('Error credentials')
+
+      setTimeout(() => {
+
+        setErrorMessage(null)
+
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  if (user === null) {
+    return (
+      <div>
+      {user === null ?
+        loginForm() :
+        <div>
+          <p>{user.name} logged</p>
+        </div>
+      }
+      </div>
+    )
+  }
 
   return (
     <div>
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,22 @@
 import axios from 'axios'
+
 const baseUrl = '/api/blogs'
 
+// 5.1
+let token = null
+
+const setToken = newToken => {
+
+  token = `bearer ${newToken}`
+
+  console.log(token)
+}
+
 const getAll = () => {
+
   const request = axios.get(baseUrl)
+
   return request.then(response => response.data)
 }
 
-export default { getAll }
+export default { getAll, setToken }
