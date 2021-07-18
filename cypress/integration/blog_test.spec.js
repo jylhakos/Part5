651dde7bbc73d596ff25e0219d56ab2c47ea169a
laptop <juha.jylhakoski@aalto.fi>
2021-07-18 12:48:25 +0800

    5.18

--- cypress/integration/blog_test.spec.js
+++ cypress/integration/blog_test.spec.js
@@ -9,7 +9,25 @@ describe('Blog app', function() {
     cy.visit('http://localhost:3000')
   })
 
+  // 5.17
   it('Login form is shown', function() {
     console.log('Login form is shown')
+  })
+
+  // 5.18
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('input[name="Username"]').type('mluukkai')
+      cy.get('input[name="Password"]').type('secret')
+      cy.get('button').contains('login').click()
+      cy.contains('Matti Luukkainen logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('input[name="Username"]').type('mluukkai')
+      cy.get('input[name="Password"]').type('salainen')
+      cy.get('button').contains('login').click()
+      cy.contains('Matti Luukkainen logged in').should('not.exist')
+    })
   })
 })
